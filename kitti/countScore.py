8346b9e4c4ab63634e155fba6d0e1ffc11642ca5
jingdao <chenjingdao@wustl.edu>
2015-07-22 17:43:33 -0400

    modified SVM pipeline

--- kitti/countScore.py
+++ kitti/countScore.py
@@ -13,6 +13,10 @@ if "-i" in sys.argv:
 	ignoreZeros = True
 else:
 	ignoreZeros = False
+if "-c" in sys.argv:
+	saveCSV = True
+else:
+	saveCSV = False
 truePositive = {}
 falsePositive = {}
 falseNegative = {}
@@ -33,8 +37,14 @@ if os.path.isfile(inputDir+'/labelCategory.txt'):
 		categories.append(l.split()[1])
 	categoryFile.close()	
 
+indexFile = open(inputDir+'/svm_prediction.txt')
+labelIndex = indexFile.readline().split()
+indexFile.close()
+
+statistics=[]
+
 for d in glob.glob(inputDir+'/*'):
-	if os.path.isdir(d) and os.path.isfile(d+'/labels.txt'):
+	if os.path.isdir(d) and os.path.isfile(d+'/labels.txt') and os.path.isfile(d+'/prediction.txt'):
 		numFiles += 1
 		if len(categories) == 0 and os.path.isfile(d+'/labelCategory.txt'):
 			categoryFile = open(inputDir+'/labelCategory.txt')
@@ -43,50 +53,56 @@ for d in glob.glob(inputDir+'/*'):
 			categoryFile.close()	
 		labels = open(d+'/labels.txt','r')
 		prediction = open(d+'/prediction.txt','r')
-		if ignoreZeros:
-			while True:
-				try:
-					l = int(labels.readline())
-				except ValueError:
-					break
-				p = int(prediction.readline())
-				if l==0 or p==0:
-					continue
-				numTrials += 1
-				if l == p:
-					increment(truePositive,l)
-				else:
-					increment(falsePositive,p)
-					increment(falseNegative,l)
-		else:
-			while True:
-				try:
-					l = int(labels.readline())
-				except ValueError:
-					break
-				p = int(prediction.readline())
-				numTrials += 1
-				if l == p:
-					increment(truePositive,l)
-				else:
-					increment(falsePositive,p)
-					increment(falseNegative,l)
+		while True:
+			try:
+				l = int(labels.readline())
+			except ValueError:
+				break
+			prob = prediction.readline().split()
+			p = int(prob[0])
+			if ignoreZeros and (l==0 or p==0):
+				continue
+			numTrials += 1
+			if l == p:
+				increment(truePositive,l)
+			else:
+				increment(falsePositive,p)
+				increment(falseNegative,l)
+			statLabel=[numTrials,categories[l],categories[p]]
+			statProb = [0] * len(categories)
+			for i in range(1,len(prob)):
+				statProb[int(labelIndex[i])] = prob[i]
+			statLabel.extend(statProb)
+			statistics.append(statLabel)
 		labels.close()
 		prediction.close()
 
 print "Processed " + str(numFiles) + " files (" + str(numTrials) + " trials)"
 print "%2s %15s %4s %8s %10s %6s" % ("","Category","Num","Accuracy","Precision","Recall")
-for i in truePositive:
+for i in range(len(categories)):
 	TP = truePositive[i] if i in truePositive else 0
 	FP = falsePositive[i] if i in falsePositive else 0
 	FN = falseNegative[i] if i in falseNegative else 0
 	num = TP + FN
 	acc = (numTrials - FP - FN) * 100.0 / numTrials
-	pre = TP * 100.0 / (TP + FP)
-	rec = TP * 100.0 / (TP + FN)
+	pre = TP * 100.0 / (TP + FP) if (TP + FP) != 0 else float('nan')
+	rec = TP * 100.0 / (TP + FN) if (TP + FN) != 0 else float('nan')
 	print "%2d %15s %4d %8.2f %10.2f %6.2f" % (i,categories[i],num,acc,pre,rec)
 
 totalAcc = sum(truePositive.values()) * 100.0 / numTrials
 totalPre = sum(truePositive.values()) * 100.0 / (sum(truePositive.values()) + sum(falsePositive.values()))
 totalRec = sum(truePositive.values()) * 100.0 / (sum(truePositive.values()) + sum(falseNegative.values()))
 print "%2s %15s %4d %8.2f %10.2f %6.2f" % ("","overall",numTrials,totalAcc,totalPre,totalRec)
+
+if saveCSV:
+	csvFile = open(inputDir+'/score.csv','w')
+	csvFile.write("%6s,%15s,%15s," % ("Sample","Actual","Predicted"))
+	for i in range(len(categories)):
+		csvFile.write("%15s," % categories[i])
+	csvFile.write('\n')
+	for s in statistics:
+		csvFile.write("%6d,%15s,%15s," % (s[0],s[1],s[2]))
+		for i in range(len(categories)):
+			csvFile.write("%15.4f," % float(s[3+i]))
+		csvFile.write('\n')
+	csvFile.close()
--- kitti/svm_pipeline.sh
+++ kitti/svm_pipeline.sh
@@ -1,41 +1,45 @@
 #!/bin/bash
 
 start=0000000000
-end=0000000010
+end=0000000120
 svm_dir=/home/jd/Downloads/libsvm-3.20
 
 scaleOption=true
-parameterOption=true
-ignoreZeroOption=true
+parameterOption=false
+ignoreZeroOption=false
 kernel=2
+svm_type=0
 
-#combine data
-rm svm_aggregate.txt
-for i in `seq -w $start $end`
+#divide into training and testing data (20/80)
+i=0
+rm svm_train_data.txt svm_test_data.txt
+for f in `seq -w $start $end` 
 do
-	cat clusters$i/svmdata.txt >> svm_aggregate.txt
+	if [ "$((i%5))" -eq 0 ]
+	then
+		cat clusters$f/svmdata.txt >> svm_train_data.txt
+	else
+		cat clusters$f/svmdata.txt >> svm_test_data.txt
+	fi
+	((i++))
 done
-
-#check data
-if ! $svm_dir/tools/checkdata.py svm_aggregate.txt
-then
-	exit 1
-fi
 
 #filter data
 if $ignoreZeroOption
 then
-	cat svm_aggregate.txt | grep -v ^0 > svm_aggregate_filtered.txt
-	mv svm_aggregate_filtered.txt svm_aggregate.txt
+	cat svm_train_data.txt | grep -v ^0 > svm_train_filtered.txt
+	mv svm_train_filtered.txt svm_train_data.txt
+	cat svm_test_data.txt | grep -v ^0 > svm_test_filtered.txt
+	mv svm_test_filtered.txt svm_test_data.txt
 fi
 
-#divide into training and testing data (20/80)
-totalLines=`wc -l svm_aggregate.txt | cut -d' ' -f1`
-subset_size=$((totalLines / 5))
-$svm_dir/tools/subset.py svm_aggregate.txt $subset_size svm_train_data.txt svm_test_data.txt
-echo "Initialize SVM: train=$subset_size test=$((totalLines-subset_size)) ..."
+#check data
+if ! $svm_dir/tools/checkdata.py svm_train_data.txt || ! $svm_dir/tools/checkdata.py svm_test_data.txt
+then
+	exit 1
+fi
+echo "Initialize SVM: train=$(wc -l < svm_train_data.txt) test=$(wc -l < svm_test_data.txt) ..."
 
- 
 #scale data
 if $scaleOption
 then
@@ -53,38 +57,56 @@ then
 	c_param=`echo $parameters | cut -d' ' -f1`
 	g_param=`echo $parameters | cut -d' ' -f2`
 	echo "SVM Train: c=$c_param g=$g_param ..."
-	$svm_dir/svm-train -c $c_param -g $g_param -t $kernel svm_train_scaled.txt model.txt
+	$svm_dir/svm-train -c $c_param -g $g_param -s $svm_type -t $kernel -b 1 svm_train_scaled.txt model.txt > /dev/null
 else
-	$svm_dir/svm-train -t $kernel svm_train_scaled.txt model.txt
+	$svm_dir/svm-train -s $svm_type -t $kernel -b 1 svm_train_scaled.txt model.txt > /dev/null
 fi
 
 #calculate output
 echo "SVM Test: (Saving to svm_prediction.txt) ..."
-$svm_dir/svm-predict svm_test_scaled.txt model.txt svm_prediction.txt
+$svm_dir/svm-predict -b 1 svm_test_scaled.txt model.txt svm_prediction.txt
 
 #save predicted values to individual directories
 i=0
-j=0
-read -a arr <<< `seq $start $end`
-rm clusters${arr[i]}/prediction.txt
-numLines=`wc -l < clusters${arr[i]}/labels.txt`
-read -a labels <<< `cat clusters${arr[i]}/labels.txt`
+j=1 #first line is label
+arr=()
+for f in `seq -w $start $end`
+do
+	if [ "$((i%5))" -ne 0 ]
+	then
+		arr=(${arr[@]} $f)
+	fi
+	((i++))
+done
+i=0
 while read line
 do
-	if $ignoreZeroOption && [ "${labels[j]}" -eq 0 ]
-	then
-		echo 0 >> clusters${arr[i]}/prediction.txt
-	else
-		echo $line >> clusters${arr[i]}/prediction.txt
-	fi
-	j=$((j+1))
-	if [ "$j" -eq "$numLines" ]
-	then
-		i=$((i+1))
-		j=0
-		rm clusters${arr[i]}/prediction.txt
-		numLines=`wc -l < clusters${arr[i]}/labels.txt`
-		read -a labels <<< `cat clusters${arr[i]}/labels.txt`
-	fi
+	predictions[i]="$line"
+	((i++))
 done < svm_prediction.txt
+for f in ${arr[@]}
+do
+	rm clusters$f/prediction.txt
+	numLines=`wc -l < clusters$f/labels.txt`
+	read -a labels <<< `cat clusters$f/labels.txt`
+	echo "Updating clusters$f/prediction.txt ..."
+	for l in ${labels[@]}
+	do
+		if $ignoreZeroOption && [ "$l" -eq 0 ]
+		then
+			echo 0 >> clusters$f/prediction.txt
+		else
+			echo ${predictions[j]} >> clusters$f/prediction.txt
+			((j++))
+		fi
+	done
+	../main clusters$f/ clusters$f/combined.pcd
+done
 
+#count score
+if $ignoreZeroOption
+then
+	./countScore.py . -i -c
+else
+	./countScore.py . -c
+fi
